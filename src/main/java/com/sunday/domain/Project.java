545688da01716b9f68fcd081e93145df4fb3d35c
syedmohin <786syedmohin@gmail.com>
2020-01-11 08:03:14 +0530

    My first Domain

--- src/main/java/com/sunday/domain/Project.java
+++ src/main/java/com/sunday/domain/Project.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
@@ -12,6 +15,9 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String projectName;
     private String projectIdentifier;
